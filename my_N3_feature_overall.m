96b32f7de0a1c6afa933d405a4eec7e353903d8c
Gi-Ren Liu <69832810+Gi-Ren-Liu@users.noreply.github.com>
2020-08-18 16:15:38 +0800

    Create my_N3_feature_overall.m

---
+++ my_N3_feature_overall.m
@@ -0,0 +1,45 @@
+function [total_feature] = my_N3_feature_overall(EEG1)
+
+Hz = 200;
+
+number_epoch = size(EEG1,2);
+
+size_node = 9;
+
+total_feature = zeros(1,size_node,number_epoch);
+
+parfor t = 1:number_epoch
+temp_L = EEG1(:,t);
+
+    H1 = temp_L>37.5;
+    H2 = temp_L<-37.5;
+    D = H1+H2;
+
+D = D(:);
+
+band_width = floor(length(D)/size_node);
+
+output = zeros(1,size_node);
+
+for k = 1:size_node
+    if (k<size_node)
+    range = (k-1)*band_width+1:k*band_width;
+    temp = D(range);
+    output(1,k) = mean(temp); 
+    else
+    range = length(D)-band_width+1:length(D);    
+    temp = D(range);
+    output(1,k) = mean(temp);
+    end
+end
+
+
+
+total_feature(:,:,t) = output;
+
+
+end
+
+total_feature(1,:,:) = total_feature/10/Hz;
+
+end
